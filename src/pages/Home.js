c406d62d8f7c52c1cd68ee492390d3708b4a41e5
mohammadsariqkhan <mohsariqkhan2004@gmail.com>
2023-08-02 15:15:53 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../features/navbar/Navbar'
+import ProductList from '../features/product-list/ProductList'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar children={<ProductList/>}/>
+    </div>
+  )
+}
+
+export default Home
